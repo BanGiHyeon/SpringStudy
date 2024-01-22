04aa9969177e0089fd958ac7830652f0be141c73
BANN <BANN@bbanggi>
2024-01-23 08:35:55 +0900

    1111

---
+++ SpringDIProject_6/src/main/java/com/sist/dao/StudentVO.java
@@ -0,0 +1,37 @@
+package com.sist.dao;
+
+public class StudentVO {
+	private int hakbum,eng,math,kor;
+	private String name;
+	public int getHakbum() {
+		return hakbum;
+	}
+	public void setHakbum(int hakbum) {
+		this.hakbum = hakbum;
+	}
+	public int getEng() {
+		return eng;
+	}
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+	public int getMath() {
+		return math;
+	}
+	public void setMath(int math) {
+		this.math = math;
+	}
+	public int getKor() {
+		return kor;
+	}
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
