31c149b69c07e48dda280611bb1c918c0b589032
BANN <BANN@bbanggi>
2024-01-23 09:00:44 +0900

    1111

---
+++ SpringAOPProject_XML/src/main/java/com/sist/dao/MyDAO2.java
@@ -0,0 +1,20 @@
+package com.sist.dao;
+
+public class MyDAO2 {
+	public void select()
+	{
+		System.out.println("오라클 => SELECT문장 요청");
+	}
+	public void insert()
+	{
+		System.out.println("오라클 => INSERT문장 요청");
+	}
+	public void update()
+	{
+		System.out.println("오라클 => UPDATE문장 요청");
+	}
+	public void delete()
+	{
+		System.out.println("오라클 => DELETE문장 요청");
+	}
+}
